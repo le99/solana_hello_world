1d6c3cc9f9bd42729f87db7b3cbc12f003fc7e3e
le99 <le.florez602@uniandes.edu.co>
2022-10-06 12:57:32 -0500

    Better maybe

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import {getProvider, initPhantom} from './auth/Auth';
 
 
 import * as web3 from '@solana/web3.js';
-
+import {Buffer} from 'buffer/';
 
 const PROGRAM_ID = "4uWRvwKL9xzdxtfTBSYc7Eh3CjMeY9CAs7Db6wXnb72a";
 const SOLANA_CLUSTER = "custom&customUrl=http%3A%2F%2Flocalhost%3A8899";
@@ -20,6 +20,7 @@ const SOLANA_CLUSTER = "custom&customUrl=http%3A%2F%2Flocalhost%3A8899";
 
 //https://solana.com/news/solana-scaffold-part-1-wallet-adapter
 
+console.log(Buffer.from([0x0]));
 
 function App() {
 
@@ -54,14 +55,45 @@ function App() {
   }
 
   async function onClick(){
+    
     let provider = getProvider();
     let pk = auth.user;
+
 
     const network = "http://127.0.0.1:8899";
 
     const connection = new web3.Connection(network);
     let payer = {publicKey: pk};
    
+
+    const counterKeypair = web3.Keypair.generate();
+    const counter = counterKeypair.publicKey;
+
+
+    const COUNTER_ACCOUNT_SIZE = 8;
+    const allocTx = web3.SystemProgram.createAccount({
+      fromPubkey: payer.publicKey,
+      newAccountPubkey: counter,
+      lamports: await connection.getMinimumBalanceForRentExemption(COUNTER_ACCOUNT_SIZE),
+      space: COUNTER_ACCOUNT_SIZE,
+      programId: new web3.PublicKey(PROGRAM_ID)
+    });
+
+
+    const tx2 = new web3.TransactionInstruction({
+      programId: new web3.PublicKey(PROGRAM_ID),
+      keys: [
+        {
+          pubkey: counter,
+          isSigner: false,
+          isWritable: true
+        }
+      ],
+      data: Buffer.from([0x0])
+    });
+
+
+
     let blockhash = (await connection.getLatestBlockhash("finalized")).blockhash;
     // create an empty transaction
     let transaction = new web3.Transaction({
@@ -70,23 +102,7 @@ function App() {
     });
 
     // add a single instruction to the transaction
-    transaction.add(
-      new web3.TransactionInstruction({
-        keys: [
-          {
-            pubkey: payer.publicKey,
-            isSigner: true,
-            isWritable: false,
-          },
-          {
-            pubkey: web3.SystemProgram.programId,
-            isSigner: false,
-            isWritable: false,
-          },
-        ],
-        programId: new web3.PublicKey(PROGRAM_ID),
-      }),
-    );
+    transaction.add(allocTx).add(tx2);
 
     // submit the transaction to the cluster
     console.log("Sending transaction...");
